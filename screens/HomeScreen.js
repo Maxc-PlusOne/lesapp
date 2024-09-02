20daa253bd2e3efc9b4bb5f4e1d5c3477fd9362b
Mpilonhle Ngcobo <mn.plusone@outlook.com>
2024-09-02 09:41:30 +0200

    - Created SOS Button - Updated expo (^51.0.0 >>>~51.0.32) - Updated react-native-svg (^15.6.0 >>> 15.2.0)

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, ActivityIndicator, Text } from 'react-native';
+import { View, StyleSheet, ActivityIndicator, Text,Pressable, Alert} from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
+import SwipeButton from '../components/SwipeButton';
+import { DummySOSButton } from '../components/DummySOSButton';
 export default function HomeScreen() {
     // Location //
     const [location, setLocation] = useState(null);
     const [errorMsg, setErrorMsg] = useState(null);
-
+    const [BtnColor, setBtnColor] = useState('red');
+    const [isActivated, setIsActivated] = useState(false);
+    const [BtnText, setBtnText ]=useState( 'Send SOS');
     useEffect(() => {
         (async () => {
             let { status } = await Location.requestForegroundPermissionsAsync();
@@ -34,12 +38,77 @@ export default function HomeScreen() {
         );
     }
 
+    function showAlert(action) {
+
+        const onPress = () => {
+
+            if (!isActivated) {
+                setIsActivated(true),
+                    setBtnColor('black'),
+                    setBtnText('Cancel')
+
+            }
+            else if (isActivated) {
+                setIsActivated(false),
+                    setBtnColor('red'),
+                    setBtnText('Send SOS')
+         
+            }
+        }
+
+        return (
+            Alert.alert(
+            'Confirmation',
+            'Are sure you want to ' + action,
+            [
+                {
+                    text: 'Cancel',
+                    style: 'cancel',
+                },
+                {
+                    text: 'Yes',
+                    onPress: onPress
+
+                }
+            ]
+            )
+        )
+
+    }
+
+    function onPress() {
+        if (!isActivated) {
+                showAlert('send SOS?')
+
+        }
+        else if (isActivated) {
+                showAlert('cancel request?')
+        }
+
+    };
+
+
 
     return (
         <View style={styles.container}>
             <View style={styles.infoBar}>
                 <Text style={styles.infoText}>Showing current address here</Text>
             </View>
+            <Pressable onPress={onPress} style={{
+                zIndex: 5,
+                backgroundColor:BtnColor,
+                width: '80%',
+                height: 48,
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderRadius: 35,
+                bottom: '14%',
+                position: 'absolute',
+
+            }}>
+                <Text style={{fontSize:24,color:'white', textStyle:'bold' }}>{BtnText}</Text>
+            </Pressable>
+
             <MapView
                 style={styles.map}
                 region={{
@@ -53,8 +122,8 @@ export default function HomeScreen() {
                     coordinate={{
                         latitude: location.coords.latitude,
                         longitude: location.coords.longitude,
-
                     }}
+
                     title="You"
                 />
             </MapView>
@@ -71,6 +140,7 @@ const styles = StyleSheet.create({
         justifyContent:'center',
         zIndex: 1
     },
+
     infoBar: {
         height: 48,
         zIndex: 3,
@@ -80,11 +150,13 @@ const styles = StyleSheet.create({
         borderRadius: 12,
         borderColor: '#DB222A',
         borderWidth: 0,
+
         // iOS shadow
         shadowColor: '#000',
         shadowOffset: { width: 0, height: 2 },
         shadowOpacity: 0.25,
         shadowRadius: 3.84,
+
         // Android shadow
         elevation: 5,
         justifyContent: 'center',
@@ -92,6 +164,7 @@ const styles = StyleSheet.create({
         margin: '5%',
         top: 0,
     },
+
     infoText: {
         color: '#5B636C',
         fontSize: 16,
