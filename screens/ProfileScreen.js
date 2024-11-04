2fc978f928794c054c790325fe60537833d93273
Mpilonhle Ngcobo <mn.plusone@outlook.com>
2024-11-04 10:19:39 +0200

    New: - Logout feature

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,11 +1,49 @@
 import * as React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import { globalStyles } from "../app/styles";
+import storageService from "../utils/storageService";
+import { CommonActions } from '@react-navigation/native';
+import { apiService } from "../utils/apiService";
 
-export default function ProfileScreen() {
+
+
+export default function ProfileScreen({navigation}) {
+    const [user, setUser] = React.useState(storageService.get('userId'));
+   async function getUser() {
+        const userId = await storageService.get('userId')
+        
+        try {
+            const res = await apiService.get('user', userId.replace(/['"]/g, ''))
+            return setUser(res)
+        } catch {
+
+        }
+    }
+
+    React.useState(() => {
+        getUser()
+    },[])
+
+    function logout() {
+
+        storageService.remove('userId'),
+            storageService.remove('isSignedIn'),
+            navigation.dispatch(
+                CommonActions.reset({
+                    index: 0,
+                    routes: [{ name: 'Welcome' }],
+                })
+            )
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.main}>
-                <Text style={styles.subtitle}> Profile Screen </Text>
+                <Text style={styles.subtitle}>{user.name}</Text>
+                <Text style={styles.subtitle}>{user.phone}</Text>
+                <Pressable style={globalStyles.btnPrimary} onPress={logout}>
+                    <Text style={globalStyles.btnText}>Logout</Text>
+                </Pressable>
             </View>
         </View>
     )
